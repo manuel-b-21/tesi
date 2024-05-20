40150db484ce9d09a942339a4547888b660e24e8
ismanuele <manuelbitto21@gmail.com>
2024-05-20 12:06:09 +0200

    Cleaning

--- 4_blocks/data_chaos/collect_plateau_values.sh
+++ 4_blocks/data_chaos/collect_plateau_values.sh
@@ -18,7 +18,7 @@ for m in `seq $mmin $mmax`; do
 				echo $m $L $p
 				for f in $lstsrc; do
 					cat $f | grep -v 'inf' | grep -v 'nan' | \
-						plateau -range 2000 4000 | tail -1 | grep -v "#" >> $dstfile
+						plateau -range 1000 4000 | tail -1 | grep -v "#" >> $dstfile
 				done
 			fi
 		done
