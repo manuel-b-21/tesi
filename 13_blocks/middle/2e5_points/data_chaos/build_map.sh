80a4f0c65d66e62addb41d245d17ded1e46cb2fd
ismanuele <manuelbitto21@gmail.com>
2024-07-02 09:49:17 +0200

    Done 12 and 13 edge

---
+++ 13_blocks/middle/2e5_points/data_chaos/build_map.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+outfile="map.dat"
+
+echo -e "#m\tL\tnu\tdnu\ta\tda" > $outfile
+
+mmin=2
+mmax=20
+Lmax=20
+
+for m in `seq $mmin $mmax`; do
+	for L in `seq 1 $Lmax`; do
+		datafile="./Averaged_plateau/plateau_"$m"_"$L".dat"
+		if [ -e $datafile ]; then
+			gnuplot -e "m=$m" -e "L=$L" fit_driver.gp >> $outfile
+		else
+			echo -e $m"\t"$L"\tNaN\tNaN\tNaN\tNaN" >> $outfile
+		fi
+	done
+	echo >> $outfile
+done
